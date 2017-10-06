7062431ee37b7dd13c618d88c09a45ae5ac5388b
thaihuynhatquang <thaihuynhatquang@gmail.com>
2017-10-06 17:58:08 +0700

    Chapter 04

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,7 @@ var ctrlLocations = require('../controllers/locations')
 var ctrlOthers = require('../controllers/others');
 
 /* Locations page. */
-router.get('/', ctrlLocations.homelist);
+router.get('/', ctrlLocations.homeList);
 router.get('/location',ctrlLocations.locationInfo);
 router.get('/location/review/new/', ctrlLocations.addReview);
 
