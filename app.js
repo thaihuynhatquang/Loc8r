cb0ec6185a59014e668506ea0ba7f68470af9abd
thaihuynhatquang <thaihuynhatquang@gmail.com>
2017-11-17 01:23:13 +0700

    Adding a dynamic front end with Angular

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('./app_api/models/db');
+var uglifyJs = require("uglify-js");
+var fs = require('fs');
 
-var routes = require('./app_server/routes/index');
+// var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 var users = require('./app_server/routes/users');
 
@@ -16,17 +18,51 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
+var appClientFiles = [
+	'app_client/app.js',
+	'app_client/home/home.controller.js',
+	'app_client/about/about.controller.js',
+	'app_client/locationDetail/locationDetail.controller.js',
+	'app_client/reviewModal/reviewModal.controller.js',
+	'app_client/common/services/loc8rData.service.js',
+	'app_client/common/services/geolocation.service.js',
+	'app_client/common/filters/formatDistance.filter.js',
+	'app_client/common/filters/addHtmlLineBreaks.filter.js',
+	'app_client/common/directives/ratingStars/ratingStars.directive.js',
+	'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+	'app_client/common/directives/navigation/navigation.directive.js',
+	'app_client/common/directives/pageHeader/pageHeader.directive.js'
+].map(function(file){
+	return fs.readFileSync(file, 'utf8')
+});
+
+var uglified = uglifyJs.minify(appClientFiles, { compress : false });
+
+fs.writeFile("public/angular/loc8r.min.js",uglified.code, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Script generated and saved: loc8r.min.js');
+	}
+});
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', routes);
+
+// app.use('/', routes);
 app.use('/api', routesApi);
 app.use('/users', users);
+
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 
 // catch 404 and forward to error handler
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,6 +4,7 @@ var ctrlLocations = require('../controllers/locations')
 var ctrlOthers = require('../controllers/others');
 
 /* Locations page. */
+router.get('/', ctrlOthers.angularApp);
 router.get('/', ctrlLocations.homeList);
 router.get('/location/:locationid',ctrlLocations.locationInfo);
 router.get('/location/:locationid/review/new', ctrlLocations.addReview);
